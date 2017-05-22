87122fb2eed8fea65fb258937f17e6a054e3f97b
askflow1111 <askflow1111@gmail.com>
2017-05-22 14:27:35 -0400

    sdfs

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 console.log('working');
 
-fetch('http://api.airbnb.com/v2/reviews?client_id=3092nxybyb0otqw18e8nh5nty&listing_id=2056659&role=all')
-  .then(function(response) {
-    response.json()
-      .then(function(lastFmData){
-        console.log(lastFmData);
-      })});
+
+
+fetch('https://api.airbnb.com/v2/reviews?client_id=3092nxybyb0otqw18e8nh5nty&listing_id=2056659&role=all')
+  .then((response) => {
+    return response.json()
+  })
+  .then((responseJson) => {
+    console.log(responseJson);
+
+  });
